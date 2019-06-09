3d617bc73ccde444c3a24e27e40e966539d34efb
Sharma <rshar144@ms.ds.uhc.com>
2019-06-09 12:24:54 +0530

    EF

--- WishList/Models/Item.cs
+++ WishList/Models/Item.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,10 @@ namespace WishList.Models
 {
     public class Item
     {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Description { get; set; }
     }
 }
